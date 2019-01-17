896dd30f90ad715199072433cf738d8f6958faeb
Ben Allfree <ben@benallfree.com>
2019-01-17 04:56:06 -0800

    Template update

--- templates/src/App.js
+++ templates/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { hot } from "react-hot-loader";
 import "./app.scss";
 
-const App = hot(module)(props => <h1>Submix says Hello!</h1>);
+const App = props => <h1>Submix says Hello!</h1>;
 
 export { App };
